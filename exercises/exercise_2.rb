141fd5a31cc3f776d42f902cb4944576206dd4cb
jswelsh <james.sinclair.welsh@gmail.com>
2019-08-22 02:09:01 +0000

    add exercise  2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@Store2 = Store.find(2)
+
+puts @store1.name
+
+@store1.name = 'Brentwood'
+
+puts @store1.name
+
